7e2df6dee809855b77dbd1ba3655cba324d8e089
Alistair Singh <alistair.singh7@gmail.com>
2015-12-10 02:17:56 +0200

    can actually parse some stuff

--- src/FSql/Parser3.fs
+++ src/FSql/Parser3.fs
@@ -5,21 +5,82 @@ module Parser3 =
 
   type State = { input: seq<char>; position: int }
 
-  type Error = Error of string
+  type Result<'a> = 
+                | Error of string
+                | Ok of 'a
+
+  type Reply<'a> = { state : State; result: Result<'a> }
 
   type Parser<'a, 'b> = Parser of (State 
-                                    -> ('a -> State -> 'b)  // OK
-                                    -> (Error -> 'b)        // Error 
+                                    -> ('a -> State -> 'b)  // Ok
+                                    -> (string -> 'b)     // Error 
                                     -> 'b) 
+  let uncons s =
+    let head = Seq.tryHead s
+    match head with
+    | None -> None
+    | Some i -> Some (i, Seq.tail s)
 
-  let parse (Parser p) state ok error = p state ok error
-
-  let pMap f p = Parser <| fun s ok error -> parse p s (ok |> f) error
+  let unParser (Parser p) state ok error = p state ok error
 
   let pReturn a = Parser <| fun s ok _ -> ok a s
 
-  let pBind m k = Parser <| fun s ok error ->
-                              let cok x s = parse (k x) s ok error
-                              parse m s cok error
+  let pPure = pReturn
 
-  let pFail msg = Parser <| fun s _  error -> error msg
+  let runParsec' p s =
+    let cok a s' = { state = s' ; result = Ok a } 
+    let cerr msg = { state = s  ; result = Error msg } 
+    unParser p s cok cerr
+
+  let runParser' p s =
+    let reply = runParsec' p s
+    let { state = s' ; result = r } = reply
+    match r with
+    | Error msg -> (s', Choice1Of2 msg)
+    | Ok a -> (s', Choice2Of2 a)
+
+  let runParser p s =
+    runParser' p s |> snd
+
+  let pMap f p = Parser <| fun s ok error -> unParser p s (ok |> f) error
+
+  let pBind m k = Parser <| fun s ok error ->
+                              let cok x s = unParser (k x) s ok error
+                              unParser m s cok error
+
+  let pFail msg = Parser <| fun _ _ error -> error msg
+
+  let pZero () = pFail "Zero Error"
+
+  let pToken nextpos test = 
+    Parser <| fun s ok error ->
+      match uncons s.input with
+      | None -> error "End Of Input"
+      | Some (c,cs) -> 
+        match test c with 
+        | Choice1Of2 err -> error err
+        | Choice2Of2 a -> 
+          let newpos = nextpos s.position c
+          let newstate = { input = cs ; position = newpos }
+          ok a newstate
+
+  let pTokens nextpos test tts =
+    match uncons tts with
+    | None -> Parser <| fun s ok _ -> ok [] s
+    | Some (_, _) -> Parser <| fun s ok error -> 
+      let rec walk ts is rs =
+        match uncons ts with
+        | None -> 
+          let pos' = nextpos s.position tts
+          let s' =  { input = rs ; position = pos' }
+          ok (List.rev is) s'
+        | Some (t, ts) ->
+          match uncons rs with
+          | None -> error "no clue"
+          | Some (c, cs) ->
+              if test t c then
+                walk ts (c::is) cs
+              else
+                error "no clue 2"
+      walk tts [] s.input
+
