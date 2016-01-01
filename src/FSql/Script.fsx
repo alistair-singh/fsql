e9fde6409211008cbde6ab877e7ae99c5f7d0756
Alistair Singh <alistair.singh7@gmail.com>
2016-01-01 02:47:25 +0200

    refined test scripts

--- src/FSql/Script.fsx
+++ src/FSql/Script.fsx
@@ -10,10 +10,10 @@ open FSql.Parser
 printfn "Basic Tests"
 
 let b1 = runParsec' (char' 'a') (initialState None "abc")
-printfn "%A" b1
+printfn "b1 - %A" b1
 
 let b2 = runParsec' (char' 'b') (initialState None "abc")
-printfn "%A" b2
+printfn "b2 - %A" b2
 
 printfn "NextStep Tests"
 
@@ -24,8 +24,19 @@ let char2 a b = parser {
                 }
 
 let res1 = runParsec' (char2 'a' 'b') (initialState None "ab")
-printfn "%A" res1
+printfn "res1 - %A" res1
 
 let res2 = runParsec' (char2 'a' 'b') (initialState None "ac")
-printfn "%A" res2
+printfn "res2 - %A" res2
+
+let char2eof a b = parser {
+                let! c1 = char2 a b
+                let! c2 = pEof ()
+                return c1 }
+
+let res3 = runParsec' (char2eof 'a' 'b') (initialState None "ab")
+printfn "res3 - %A" res3
+
+let res4 = runParsec' (char2eof 'a' 'b') (initialState None "ab ")
+printfn "res4 - %A" res4
 
